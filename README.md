1f40bf0643f255cc8d58c9a831679f00ad1ed024
Paul Kerr <paul.kerr@securigroup.co.uk>
2022-08-02 09:40:58 +0100

    upload all code

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # pelion-technical
 
+## Requirements
+```
+Node.js
+npm
+Vue (v3.x)
+```
+
 ## Project setup
 ```
 npm install
@@ -15,10 +22,21 @@ npm run serve
 npm run build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
+## The Application
+The app provides users with a list of activities to try out. Clicking on the "Get Suggestion" button will add a new suggestion to the list. The "Clear Suggestion" button will clear the full list. The table data can be sorted by clicking on the table headers.
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+The app is built in Vue 3 using the Quasar UI framework.
+
+![Image 1](https://i.imgur.com/G3zryDy.png)
+
+The app also provides the user with a number of filters for specifying what type of activities they would like suggested. To activate a filter simply expand it's container and select a value. The filter will then be reflected in the suggestions provides by the "Get Suggestion" button.
+
+![Image 2](https://i.imgur.com/KFqvp8K.png)
+
+## Potential Improvements
+
+The app can be improved by adding the following features:
+
+  * Dark theme
+  * Ability to check off list items
+  * Ability to remove single items from list
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,367 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <q-layout view="lHh Lpr lFf">
+      <q-header elevated>
+        <q-toolbar>
+          <q-toolbar-title> Activity Suggestion App </q-toolbar-title>
+          <q-btn
+            flat
+            dense
+            round
+            label="Filters"
+            aria-label="Menu"
+            @click="rightDrawerOpen = !rightDrawerOpen"
+          />
+        </q-toolbar>
+      </q-header>
+      <q-drawer
+        side="right"
+        v-model="rightDrawerOpen"
+        bordered
+        content-class="bg-grey-1"
+      >
+        <q-scroll-area class="fit">
+          <q-form @submit="updateFilters" class="q-gutter-md" ref="filterForm">
+            <q-expansion-item
+              group="filterGroup"
+              expand-separator
+              v-model="typeFilter"
+              label="Filter by Activity Type"
+            >
+              <q-card>
+                <q-card-section>
+                  <q-select
+                    outlined
+                    v-model="typeModel"
+                    :options="types"
+                    label="Activity Type"
+                  />
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+            <q-expansion-item
+              group="filterGroup"
+              expand-separator
+              v-model="participantFilter"
+              label="Filter by Number of Participants"
+            >
+              <q-card>
+                <q-card-section>
+                  <q-select
+                    outlined
+                    v-model="participantModel"
+                    :options="participants"
+                    label="Number of Participants"
+                  />
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+            <q-expansion-item
+              group="filterGroup"
+              expand-separator
+              v-model="accessibiltyFilter"
+              label="Filter by Accessibilty"
+            >
+              <q-card>
+                <q-card-section>
+                  <q-slider
+                    v-model="accessibiltyModel"
+                    :min="0"
+                    :max="1"
+                    :step="0.01"
+                    label
+                    :marker-labels="fnMarkerLabel"
+                    :markers="0.1"
+                  />
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+            <q-expansion-item
+              group="filterGroup"
+              expand-separator
+              v-model="accessibiltyRangeFilter"
+              label="Filter by Accessibility Range"
+            >
+              <q-card>
+                <q-card-section>
+                  <q-range
+                    v-model="accessibiltyRangeModel"
+                    :min="0"
+                    :max="1"
+                    :step="0.01"
+                    color="green"
+                    label
+                    :marker-labels="fnMarkerLabel"
+                    :markers="0.25"
+                  />
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+            <q-expansion-item
+              group="filterGroup"
+              expand-separator
+              v-model="priceFilter"
+              label="Filter by Price"
+            >
+              <q-card>
+                <q-card-section>
+                  <q-slider
+                    v-model="priceModel"
+                    :min="0"
+                    :max="1"
+                    :step="0.01"
+                    label
+                    :marker-labels="fnMarkerLabel"
+                    :markers="0.25"
+                  />
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+            <q-expansion-item
+              group="filterGroup"
+              expand-separator
+              v-model="priceRangeFilter"
+              label="Filter by Price Range"
+            >
+              <q-card>
+                <q-card-section>
+                  <q-range
+                    v-model="priceRangeModel"
+                    :min="0"
+                    :max="1"
+                    :step="0.01"
+                    color="green"
+                    label
+                    :marker-labels="fnMarkerLabel"
+                    :markers="0.25"
+                  />
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+            <!--<q-btn label="Submit" type="submit" color="primary" />
+            <div>
+                <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+              </div>-->
+          </q-form>
+        </q-scroll-area>
+      </q-drawer>
+
+      <q-page-container>
+        <div class="q-pa-md q-gutter-sm">
+          <q-btn
+            color="purple"
+            @click="getSuggestion()"
+            label="Get Suggestion"
+          />
+          <q-btn
+            color="red"
+            @click="clearSuggestions()"
+            label="Clear Suggestions"
+          />
+          <!--<div v-if="suggestion">-->
+          <q-table
+            style="margin-top: 15px"
+            title="Suggestions"
+            :rows="rows"
+            :columns="columns"
+            :pagination="pagination"
+            row-key="name"
+          />
+        </div>
+      </q-page-container>
+      <!--</div>-->
+    </q-layout>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  setup() {
+    const filterForm = ref(null);
+    return {
+      filterForm,
+      typeFilter: ref(false),
+      participantFilter: ref(false),
+      accessibiltyFilter: ref(false),
+      accessibiltyRangeFilter: ref(false),
+      priceFilter: ref(false),
+      priceRangeFilter: ref(false),
+      fnMarkerLabel: (val) => parseFloat(val).toFixed(2),
+      accessibiltyModel: ref(0.4),
+      priceModel: ref(0.5),
+      accessibiltyRangeModel: ref({
+        min: 0.2,
+        max: 0.7,
+      }),
+      priceRangeModel: ref({
+        min: 0.2,
+        max: 0.7,
+      }),
+      typeModel: ref("busywork"),
+      participantModel: ref(1),
+      rightDrawerOpen: ref(false),
+      types: [
+        "busywork",
+        "diy",
+        "education",
+        "social",
+        "recreational",
+        "relaxation",
+        "music",
+        "cooking",
+        "charity",
+      ],
+      participants: [1, 2, 3, 4, 5, 6, 7, 8],
+      pagination: {
+        rowsPerPage: 0,
+        sortBy: "id",
+        descending: true,
+      },
+      columns: [
+        {
+          name: "id",
+          required: true,
+          label: "#",
+          align: "left",
+          field: "id",
+          sortable: true,
+        },
+        {
+          name: "activity",
+          label: "Activity",
+          align: "left",
+          field: "activity",
+          sortable: true,
+        },
+        {
+          name: "accessibility",
+          align: "center",
+          label: "Accessibility",
+          field: "accessibility",
+          sortable: true,
+        },
+        { name: "type", label: "Type", field: "type", sortable: true },
+        {
+          name: "participants",
+          label: "Participants",
+          field: "participants",
+          sortable: true,
+        },
+        { name: "price", label: "Price", field: "price", sortable: true },
+        { name: "key", label: "Key", field: "key", sortable: true },
+      ],
+    };
+  },
+  data() {
+    return {
+      rows: [],
+    };
+  },
+  methods: {
+    clearSuggestions() {
+      this.rows = [];
+      localStorage.clear();
+    },
+    getSuggestion() {
+      const filter = this.updateFilters();
+      console.log({ filter });
+      let apiUrl = "http://www.boredapi.com/api/activity";
+      if (Object.keys(filter).length) {
+        let apiSuffix = "";
+        switch (filter.name) {
+          case "typeFilter":
+            apiSuffix = `?type=${filter.value}`;
+            break;
+          case "participantFilter":
+            apiSuffix = `?participants=${filter.value}`;
+            break;
+          case "accessibiltyFilter":
+            apiSuffix = `?accessibility=${filter.value}`;
+            break;
+          case "accessibiltyRangeFilter":
+            apiSuffix = `?minaccessibility=${filter.value.min}&maxaccessibility=${filter.value.max}`;
+            break;
+          case "priceRangeFilter":
+            apiSuffix = `?minprice=${filter.value.min}&maxprice=${filter.value.max}`;
+            break;
+          case "priceFilter":
+            apiSuffix = `?price=${filter.value}`;
+            break;
+          default:
+            apiSuffix = "";
+        }
+        apiUrl = apiUrl + apiSuffix;
+      }
+      fetch(apiUrl)
+        .then((res) => res.json())
+        .then((data) => {
+          let id = this.rows.length + 1;
+          data.id = id;
+          data.activity ? this.rows.push(data) : console.log("No data");
+          localStorage.clear();
+          localStorage.setItem("rows", JSON.stringify(this.rows));
+        })
+        .catch((err) => console.log(err));
+    },
+    updateFilters() {
+      let activeFilter = {};
+      const filterList = [
+        {
+          name: "typeFilter",
+          active: this.typeFilter,
+          value: this.typeModel,
+        },
+        {
+          name: "participantFilter",
+          active: this.participantFilter,
+          value: this.participantModel,
+        },
+        {
+          name: "accessibiltyFilter",
+          active: this.accessibiltyFilter,
+          value: this.accessibiltyModel,
+        },
+        {
+          name: "accessibiltyRangeFilter",
+          active: this.accessibiltyRangeFilter,
+          value: this.accessibiltyRangeModel,
+        },
+        {
+          name: "priceRangeFilter",
+          active: this.priceRangeFilter,
+          value: this.priceRangeModel,
+        },
+        {
+          name: "priceFilter",
+          active: this.priceFilter,
+          value: this.priceModel,
+        },
+      ];
+      filterList.forEach((filter) => {
+        if (filter.active) activeFilter = filter;
+      });
+      console.log({ activeFilter });
+      return activeFilter;
+    },
+  },
+  mounted() {
+    // this.getSuggestion();
+    let storedRows = localStorage.getItem("rows");
+    if (storedRows !== null) {
+      this.rows = JSON.parse(storedRows);
+    } else {
+      this.getSuggestion();
+    }
+    console.log(storedRows);
+  },
+};
 </script>
